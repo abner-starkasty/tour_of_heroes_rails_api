55860d9fc035dcdb78634caaa757a5f752e26eed
Abner Willys <abwillys001@gmail.com>
2021-12-25 01:03:38 -0300

    Create seeds with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Hero.delete_all # Antes de executar o seed deleta tudo
+
+5.times do
+  Hero.create name: Faker::Superhero.name
+end
